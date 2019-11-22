1353541dc0b15efb5e7dbf8b80379564c35b346e
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2019-11-22 17:13:24 +0700

    Add project files.

---
+++ dotnet-core-simple-api/Controllers/SimpleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_core_simple_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleController : ControllerBase
+    {
+        // GET: api/Simple
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", ".NET Core API" };
+        }
+
+        // GET: api/Simple/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "Hello API";
+        }
+    }
+}
